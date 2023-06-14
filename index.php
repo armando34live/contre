f8bae52c5599881eccf40850a77c665448310982
armando34 <armando.mateus@taxationmind.com.br>
2023-06-14 08:51:10 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <title>estrutura de controle IF com PHP</title>
+</head>
+<!-- As estrutura de controle são instruções 
+que possibilitam monitorar o fluxo do programa.-->
+<body>
+
+<?php
+ $a = 2;
+
+ if($a == 1){
+    
+    echo "Sacar o dinheiro. <br>";
+ }
+
+ if ($a == 2) {
+    echo "Depositar dinheiro. <br>";
+ }
+?>
+
+
+</body>
+</html>
